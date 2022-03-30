2257d9b90c96700bdb7ff78708909eff19abe10b
Jorge <georgevazj@gmail.com>
2022-03-30 11:33:58 +0200

    Lorem ipsum message

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
+var message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat odio sed lobortis finibus. Pellentesque posuere rutrum ultrices. Praesent maximus.'
 app.get('/', function(req, res) {
-    res.send('Hola Mundo!');
+    res.send(message);
 });
 
 var server = app.listen(3000, function() {
